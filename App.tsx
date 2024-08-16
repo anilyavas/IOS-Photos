8e9d0bbb2dbd7beb3470d9324133ed02b17cec57
anilyavas <anil.yavas99@gmail.com>
2024-08-16 17:38:41 +0300

    header dots

--- App.tsx
+++ App.tsx
@@ -1,18 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import {
   StyleSheet,
-  View,
   FlatList,
   Image,
-  Text,
   ScrollView,
   useWindowDimensions,
+  View,
+  Text,
+  NativeScrollEvent,
+  NativeSyntheticEvent,
 } from 'react-native';
 import { photos } from './data';
 import Carousel from './Carousel';
+import { useState } from 'react';
 
 export default function App() {
   const { height, width } = useWindowDimensions();
+  const [headerCarouselPage, setHeaderCarouselPage] = useState(0);
+
+  const onHeaderCarouselScroll = (
+    e: NativeSyntheticEvent<NativeScrollEvent>
+  ) => {
+    const currentPage = Math.max(0, e.nativeEvent.contentOffset.x / width);
+    if (currentPage != headerCarouselPage) {
+      setHeaderCarouselPage(currentPage);
+    }
+  };
 
   return (
     <ScrollView style={styles.container} showsHorizontalScrollIndicator={false}>
@@ -23,6 +36,7 @@ export default function App() {
         snapToInterval={width}
         snapToAlignment='start'
         decelerationRate={'fast'}
+        onScroll={onHeaderCarouselScroll}
       >
         <FlatList
           style={{ width }}
@@ -37,10 +51,30 @@ export default function App() {
           )}
         />
         <Image
+          source={photos[0].image}
+          style={{ width, height: '100%', resizeMode: 'cover' }}
+        />
+        <Image
           source={photos[10].image}
           style={{ width, height: '100%', resizeMode: 'cover' }}
         />
       </ScrollView>
+      <View style={styles.dotContainer}>
+        {Array(3)
+          .fill(0)
+          .map(({ item, index }) => (
+            <View
+              style={{
+                width: index === headerCarouselPage ? 10 : 8,
+                aspectRatio: 1,
+                backgroundColor:
+                  index === headerCarouselPage ? 'black' : 'grey',
+                borderRadius: 5,
+                alignItems: 'center',
+              }}
+            />
+          ))}
+      </View>
       <Carousel photos={photos.slice(0, 3)} title='Albums' />
       <Carousel photos={photos.slice(3, 6)} title='People' />
       <Carousel photos={photos.slice(6, 9)} title='Featured' />
@@ -58,5 +92,12 @@ const styles = StyleSheet.create({
   photo: {
     width: `${100 / 4}%`,
     aspectRatio: 1,
+  },
+  dotContainer: {
+    padding: 10,
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    gap: 5,
   },
 });
